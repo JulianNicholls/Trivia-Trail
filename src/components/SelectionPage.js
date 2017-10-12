3cdf1debb1f87d6d46dc1eb59ef4c875047ce8e3
Julian Nicholls <juliannicholls29@gmail.com>
2017-10-12 22:14:49 +0100

    Add submission to SelectionPage

--- src/components/SelectionPage.js
+++ src/components/SelectionPage.js
@@ -4,8 +4,10 @@ const CATEGORIES_URL = 'https://opentdb.com/api_category.php';
 
 class SelectionPage extends Component {
   state = {
-    categories:  [{id: 0, name: 'Loading...'}],
-    count:       10
+    categories:    [{id: 0, name: 'Loading...'}],
+    category:      0,
+    difficulty:    'any',
+    count:         10
   };
 
   async componentWillMount() {
@@ -20,6 +22,16 @@ class SelectionPage extends Component {
     this.setState(() => ({ categories }));
   }
 
+  handleCategory = (evt) => {
+    const category = evt.target.value;
+    this.setState(() => ({ category  }));
+  }
+
+  handleDifficulty = (evt) => {
+    const difficulty = evt.target.value;
+    this.setState(() => ({ difficulty }));
+  }
+
   onCountChanged = (evt) => {
     const countText = evt.target.value;
     let   count     = parseInt(countText, 10);
@@ -29,23 +41,25 @@ class SelectionPage extends Component {
     else if (count > 50)
       count = 50;
 
-    console.log(count);
-
     this.setState(() => ({ count }));
+  }
+
+  handleGo = () => {
+    this.props.handleSelect(this.state);
   }
 
   render() {
     return (
       <form className="selection-form">
         <label htmlFor="category">Category</label>
-        <select id="category">
+        <select id="category" value={this.state.category} onChange={this.handleCategory}>
           {this.state.categories.map(({ id, name }, idx) => (
             <option key={idx} value={id}>{name}</option>
           ))}
         </select><br />
 
         <label htmlFor="difficulty">Difficulty</label>
-        <select id="difficulty">
+        <select id="difficulty" value={this.state.difficulty} onChange={this.handleDifficulty}>
           <option value="any">Any Difficulty</option>
           <option value="easy">Easy</option>
           <option value="medium">Medium</option>
@@ -53,9 +67,11 @@ class SelectionPage extends Component {
         </select><br />
 
         <label htmlFor="count">Number of Questions</label>
-        <input type="number" value={this.state.count} onChange={this.onCountChanged} />
+        <input type="number" value={this.state.count} onChange={this.onCountChanged} /><br />
+
+        <button className="button" type="submit" onClick={this.handleGo}>GO!</button>
       </form>
-    )
+    );
   }
 };
 
