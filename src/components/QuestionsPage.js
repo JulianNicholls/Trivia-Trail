a384a6c4f34568042a2844723c71771af91e0735
Julian Nicholls <juliannicholls29@gmail.com>
2017-10-16 09:28:25 +0100

    Change requested encoding to url3986 to allow easier decoding
    decodeURIComponent will decode the encoding.
    Add explicit loading state item

--- src/components/QuestionsPage.js
+++ src/components/QuestionsPage.js
@@ -3,10 +3,11 @@ import React, { Component }     from 'react';
 import Question                 from './Question';
 import ResultsSummary           from './ResultsSummary';
 
-const QUESTIONS_URL = 'https://opentdb.com/api.php';
+const QUESTIONS_URL = 'https://opentdb.com/api.php?encode=url3986';
 
 class QuestionsPage extends Component {
   state = {
+    loading:   true,
     questions: [],
     index:     0,
     answers:   []
@@ -15,7 +16,7 @@ class QuestionsPage extends Component {
   async componentWillMount() {
     const { category, difficulty, count } = this.props;
 
-    const baseURL       = QUESTIONS_URL + `?amount=${count}`;
+    const baseURL       = QUESTIONS_URL + `&amount=${count}`;
     const categoryStr   = (category   !== '0')   ? `&category=${category}` : '';
     const difficultyStr = (difficulty !== 'any') ? `&difficulty=${difficulty}` : '';
 
@@ -26,9 +27,21 @@ class QuestionsPage extends Component {
 
     const questions     = [];
 
-    data.results.forEach((question) => questions.push(question));
+    data.results.forEach((question) => questions.push(this.processed(question)));
 
-    this.setState(() => ({ questions }));
+    this.setState(() => ({ loading: false, questions }));
+  }
+
+  decodeTrim(text) {
+    return decodeURIComponent(text).trim();
+  }
+
+  processed(question) {
+    question.question           = this.decodeTrim(question.question);
+    question.correct_answer     = this.decodeTrim(question.correct_answer);
+    question.incorrect_answers  = question.incorrect_answers.map(text => this.decodeTrim(text));
+
+    return question;
   }
 
   header() {
@@ -37,7 +50,7 @@ class QuestionsPage extends Component {
 
     return (
       <div className="questions__header">
-        <span className="questions__header__index">Question {index + 1} of {questions.length}</span>
+        {index < questions.length && <span className="questions__header__index">Question {index + 1} of {questions.length}</span>}
         {index > 0 && <span className="questions__header__index">{correct} correct</span>}
       </div>
     );
@@ -55,21 +68,16 @@ class QuestionsPage extends Component {
   }
 
   page() {
-    const { questions, index, answers } = this.state;
+    const { loading, questions, index, answers } = this.state;
 
-    if (questions.length === 0)
+    if (loading)
       return;
 
     if (index < questions.length) {
-      return (
-        <Question
-          {...this.state.questions[this.state.index]}
-          sendAnswer={this.receiveAnswer}
-        />
-      );
+      return <Question {...questions[index]} sendAnswer={this.receiveAnswer} />;
     }
 
-    return <ResultsSummary questions={questions} answers={answers} />;
+    return <ResultsSummary questions={questions} answers={answers} reset={this.props.reset}/>;
   }
 
   render() {
