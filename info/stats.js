47a42fccf02e9f380c929003ed464cd9b8df67ce
Julian Nicholls <juliannicholls29@gmail.com>
2018-02-15 11:46:51 +0000

    Improve stats display

--- info/stats.js
+++ info/stats.js
@@ -1,29 +1,29 @@
 const fetch = require('node-fetch');
+const { ljust, rjust } = require('justify-text');
 
-
-const CATEGORIES_URL         = 'https://opentdb.com/api_category.php';
+const CATEGORIES_URL = 'https://opentdb.com/api_category.php';
 const CATEGORY_QUESTIONS_URL = 'https://opentdb.com/api_count.php?category=';
 
 let categories = [];
 
 const load_categories = async () => {
-  const raw  = await fetch(CATEGORIES_URL);
+  const raw = await fetch(CATEGORIES_URL);
   const data = await raw.json();
 
   return data;
 };
 
-const get_question_count = async (id) => {
-  const raw  = await fetch(CATEGORY_QUESTIONS_URL + id);
+const get_question_count = async id => {
+  const raw = await fetch(CATEGORY_QUESTIONS_URL + id);
   const data = await raw.json();
 
   return data;
 };
 
-load_categories().then((data) => {
-  data.trivia_categories.forEach((cat) => {
-//    console.log(`id: ${cat.id}, ${cat.name}`);
-    get_question_count(cat.id).then((cat_info) => {
+load_categories().then(data => {
+  data.trivia_categories.forEach(cat => {
+    //    console.log(`id: ${cat.id}, ${cat.name}`);
+    get_question_count(cat.id).then(cat_info => {
       const {
         total_question_count: total,
         total_easy_question_count: easy,
@@ -31,16 +31,31 @@ load_categories().then((data) => {
         total_hard_question_count: hard
       } = cat_info.category_question_count;
 
-//      console.log(cat.id, cat_info);
+      //      console.log(cat.id, cat_info);
       categories.push({
-        id:     cat_info.category_id,
-        name:   cat.name,
-        total, easy, medium, hard
+        id: cat_info.category_id,
+        name: cat.name,
+        total,
+        easy,
+        medium,
+        hard
       });
     });
   });
 });
 
 setTimeout(() => {
-  console.log(categories);
+  categories
+    .sort((cata, catb) => cata.id - catb.id)
+    .map(
+      cat =>
+        `${rjust(cat.id.toString(), 3)}, ${ljust(cat.name, 40)}  Easy: ${rjust(
+          cat.easy.toString(),
+          3
+        )}  Medium: ${rjust(cat.medium.toString(), 3)}  Hard: ${rjust(
+          cat.hard.toString(),
+          3
+        )}  Total: ${rjust(cat.total.toString(), 3)}`
+    )
+    .forEach(item => console.log(item));
 }, 10000);
