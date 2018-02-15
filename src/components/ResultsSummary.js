76f8af3bc0cdad348b5b7e3afa34f5dcd63d30cc
Julian Nicholls <juliannicholls29@gmail.com>
2018-02-15 11:48:56 +0000

    Re-factor results summary into a CSS grid

--- src/components/ResultsSummary.js
+++ src/components/ResultsSummary.js
@@ -1,22 +1,46 @@
-import React     from 'react';
+import React from 'react';
 
-const ResultQuestion = ({ correct, text, question }) => (
-  <div className={`result result__correct-${correct}`}>
-    <div>{question.question}</div>
-    <div>Your Answer: {text} {correct && '✔︎'}</div>
-    {!correct && <div>Correct answer: {question.correct_answer}</div>}
-  </div>
-);
-
-const ResultsSummary = ({ questions, answers, reset }) => {
-  return (
-    <div className="results">
-      {answers.map(({ correct, text }, idx) => (
-        <ResultQuestion correct={correct} text={text} question={questions[idx]} key={idx} />
-      ))}
-      <button className="button button--right" onClick={reset}>Restart</button>
+const ResultQuestion = ({ correct, text, question }) => {
+  const user_answer = correct ? (
+    <div key="c" className="correct result__correct-true">
+      {text} ︎︎&#10003;
+    </div>
+  ) : (
+    <div key="w" className="result__correct-false">
+      {text}
     </div>
   );
+
+  const correct_answer = !correct && (
+    <div key="wc" className="correct">
+      {question.correct_answer}
+    </div>
+  );
+
+  return [
+    <div key="q" className="result__question">
+      {question.question}
+    </div>,
+    user_answer,
+    correct_answer
+  ];
 };
+
+const ResultsSummary = ({ questions, answers, reset }) => (
+  <div className="results">
+    {answers.map(({ correct, text }, idx) => (
+      <ResultQuestion
+        correct={correct}
+        text={text}
+        question={questions[idx]}
+        key={idx}
+      />
+    ))}
+
+    <button className="button button--right" onClick={reset}>
+      Restart
+    </button>
+  </div>
+);
 
 export default ResultsSummary;
