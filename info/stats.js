32d8557e1c73c0c1efed214881ff6dbe8e6638a6
Julian Nicholls <juliannicholls29@gmail.com>
2020-01-17 19:19:02 +0000

    Change to use axios for info

--- info/stats.js
+++ info/stats.js
@@ -1,5 +1,5 @@
-const fetch = require('node-fetch');
 const { ljust, rjust } = require('justify-text');
+const axios = require('axios');
 
 const CATEGORIES_URL = 'https://opentdb.com/api_category.php';
 const CATEGORY_QUESTIONS_URL = 'https://opentdb.com/api_count.php?category=';
@@ -7,17 +7,15 @@ const CATEGORY_QUESTIONS_URL = 'https://opentdb.com/api_count.php?category=';
 let categories = [];
 
 const load_categories = async () => {
-  const raw = await fetch(CATEGORIES_URL);
-  const data = await raw.json();
+  const response = await axios.get(CATEGORIES_URL);
 
-  return data;
+  return response.data;
 };
 
 const get_question_count = async id => {
-  const raw = await fetch(CATEGORY_QUESTIONS_URL + id);
-  const data = await raw.json();
+  const response = await axios.get(CATEGORY_QUESTIONS_URL + id);
 
-  return data;
+  return response.data;
 };
 
 load_categories().then(data => {
@@ -28,17 +26,16 @@ load_categories().then(data => {
         total_question_count: total,
         total_easy_question_count: easy,
         total_medium_question_count: medium,
-        total_hard_question_count: hard
+        total_hard_question_count: hard,
       } = cat_info.category_question_count;
 
-      //      console.log(cat.id, cat_info);
       categories.push({
         id: cat_info.category_id,
         name: cat.name,
         total,
         easy,
         medium,
-        hard
+        hard,
       });
     });
   });
@@ -49,13 +46,13 @@ setTimeout(() => {
     .sort((cata, catb) => cata.id - catb.id)
     .map(
       cat =>
-        `${rjust(cat.id.toString(), 3)}, ${ljust(cat.name, 40)}  Easy: ${rjust(
-          cat.easy.toString(),
+        `${rjust(cat.id, 3)}: ${ljust(cat.name, 40)}  Easy: ${rjust(
+          cat.easy,
           3
-        )}  Medium: ${rjust(cat.medium.toString(), 3)}  Hard: ${rjust(
-          cat.hard.toString(),
+        )}   Medium: ${rjust(cat.medium, 3)}   Hard: ${rjust(
+          cat.hard,
           3
-        )}  Total: ${rjust(cat.total.toString(), 3)}`
+        )}   Total: ${rjust(cat.total, 3)}`
     )
     .forEach(item => console.log(item));
 }, 10000);
